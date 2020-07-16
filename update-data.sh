c4fd8c90ebfc3fba05ccd53abff5a1515e54b849
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-07-16 14:12:02 -0700

    Update update-data.sh

--- update-data.sh
+++ update-data.sh
@@ -1,7 +1,4 @@
-rm -rf Data/time_series_covid19_confirmed_US.csv
 rm -rf Data/state_data.csv
-
-wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv
 
 #python CovidNetAI.py
 
