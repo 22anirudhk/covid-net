419a2cb13f04d960528789df72907f714f513d3a
22anirudhk <45473342+22anirudhk@users.noreply.github.com>
2020-07-17 11:38:29 -0700

    Update update-data.sh

--- update-data.sh
+++ update-data.sh
@@ -6,12 +6,15 @@ rm -rf "Data/state_data.csv"
 
 python "CovidNetAI.py"
 
+ssh -T git@github.com
+
+git remote set-url origin git@github.com:22anirudhk/Hackathon-Secret.git
+
 git add .
 
-current_date=$(date +'%m/%d/%Y')
+current_date=$(date +'%c')
 git commit -m "Update predictions. ${current_date}"
 
 git push
 
 popd
-
