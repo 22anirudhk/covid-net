1896cc7a905fb414a5d9bbc0f7975bb00a7380b3
22anirudhk <22anirudhk@students.harker.org>
2020-07-23 18:25:46 +0000

    Automated Update Predictions.

--- update-data.sh
+++ update-data.sh
@@ -10,10 +10,12 @@ ssh -T git@github.com
 
 git remote set-url origin git@github.com:22anirudhk/Hackathon-Secret.git
 
-git add .
+git add "Data/state_data.csv"
+git add "Data/predicted_data.csv"
+git add "Models/*"
 
 current_date=$(date +'%c')
-git commit -m "Update predictions. ${current_date}"
+git commit -m "Automated Predictions Update. ${current_date}"
 
 git push
 
